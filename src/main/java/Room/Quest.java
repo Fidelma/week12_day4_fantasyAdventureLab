ccdcab2e14c879db4c70420c0941ba28ef48529e
Fidelma <fidelma.beagan@gmail.com>
2019-09-15 10:32:59 +0100

    quest added

---
+++ src/main/java/Room/Quest.java
@@ -0,0 +1,12 @@
+package Room;
+
+import java.util.ArrayList;
+
+public class Quest {
+
+    private ArrayList<Room> rooms;
+
+    public Quest(){
+        this.rooms = new ArrayList<Room>();
+    }
+}
